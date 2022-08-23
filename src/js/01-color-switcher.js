617155466e5f3a8109e3f5d1f54c04eedd6f2bfd
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-08-23 20:49:05 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+  bodyEl: document.body,
+};
+let timerId = null;
 
+refs.startButton.addEventListener('click', startChangeColorBody);
+refs.stopButton.addEventListener('click', stopChangeColorBody);
+
+function startChangeColorBody() {
+  timerId = setInterval(() => {
+    const bgColor = getRandomHexColor();
+    refs.bodyEl.style.backgroundColor = bgColor;
+  }, 1000);
+  refs.startButton.disabled = true;
+}
+function stopChangeColorBody() {
+  clearInterval(timerId);
+  refs.startButton.disabled = false;
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
